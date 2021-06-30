011f6207c1bc15de9bdb6aee43e7e8cc16e183f8
leonjovanovic <leonjovanovic1996@gmail.com>
2021-07-01 00:02:50 +0200

    Continue working on linear regression

--- linear_regression/main.py
+++ linear_regression/main.py
@@ -1,37 +1,32 @@
 import json
-
 import numpy as np
+from sklearn.utils import shuffle
 
 HYPERPARAMETERS = {
-    'num_features': 5,
-    'features': ('lokacija2', 'kvadratura', 'uknjizenost', 'tip_grejanja', 'broj_soba'),
+    'num_features': 7,
+    'features': ('lokacija2', 'kvadratura', 'sprat', 'broj_soba', 'parking', 'lift', 'terasa'),
     'learning_rate': 0.01,
-    'random_seed': 1,
+    'random_seed': 0,
     'data_size': 100828
 }
 #-------------------------------------------PREPROCESSING DATA----------------------------------------------------------
-# 1) Napraviti matricu broj_podataka x broj_osobina ---------- NE SME DA BUDE NONE
-data = np.zeros([HYPERPARAMETERS['data_size'], HYPERPARAMETERS['num_features']])
-i = 0
-with open("../data/data_real_estates.json", 'r') as infile:
+# I Create and import data and output
+with open("../data/data_flats_sale.json", 'r') as infile:
     json_data = json.load(infile)
-    for r in json_data:
-        j = 0
-        for feature in HYPERPARAMETERS['features']:
-            data[i, j] = r[feature]
-            j += 1
-        i += 1
-# 2) Srediti podatke - normalizovati i izmesati
-# II Normalization
+data = np.zeros([len(json_data), HYPERPARAMETERS['num_features']])
+output = np.zeros([len(json_data), 1])
 i = 0
-for _ in HYPERPARAMETERS['features']:
-    mean = np.mean(data[:, i])
-    sigma = np.sqrt(np.mean(data[:, i]**2))
-    data[:, i] = (data[:, i] - mean)/sigma
+for r in json_data:
+    j = 0
+    for feature in HYPERPARAMETERS['features']:
+        data[i, j] = r[feature]
+        j += 1
+    output[i, 0] = r['cena']
     i += 1
+# II Normalization (Standardization)
+data = (data - np.mean(data, axis=0)) / np.std(data, axis=0)
 # III Shuffling data
-
-
+data, output = shuffle(data, output, random_state=HYPERPARAMETERS['random_seed'])
 # 3) Podeliti podatke u train, dev, test skupove (0.6, 0.2, 0.2)
 # 4) Ugnjezdena unakrsna validacija struktura
 # -- Gradijentni spust
